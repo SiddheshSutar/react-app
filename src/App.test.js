1fd532782a7b66b140e6b5df542dc7b3ca659ea8
Siddhesh Sutar <siddhesh.sutar@mphasis.com>
2019-09-03 13:25:35 +0530

    edit test.js with some detail

--- src/App.test.js
+++ src/App.test.js
@@ -4,15 +4,26 @@ import {mount} from 'enzyme';
 
 it('render app', () => {
 
+  // 1
   let headerDiv = document.createElement('div');
   headerDiv.id = 'header';
   let rootDiv = document.createElement('div');
   rootDiv.id = 'root';
-  // let documentMock = document.createElement('div');
   document.body.appendChild(rootDiv);
   document.body.appendChild(headerDiv);
 
   const rendered = mount(<App /> , {attachTo : document.body}); 
+  // 1
+
+  // 2
+  // tried this also 
+
+  // document.body.innerHTML = '<div id="root"><div id="header"></div></div>';
+  // const rendered = mount(<App />, {attachTo : document.querySelector('#root')});
+
+  // 2 ENDS
+
+  // random line which never executes , code is breaking in mount only
   expect(rendered.find('.div_div')).text.toBe('dd');
 
 });
